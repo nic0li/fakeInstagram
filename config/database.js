ddb31d5087c0bfadf251086d1d854cff5f08c57c
Nicoli <nicoli1992@gmail.com>
2020-07-21 18:40:46 -0300

    renomeando diretorios

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
 };
